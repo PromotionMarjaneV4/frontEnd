dce74a585e870560d29924288e5718cf1800d203
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-11-30 11:21:10 +0100

    update tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
