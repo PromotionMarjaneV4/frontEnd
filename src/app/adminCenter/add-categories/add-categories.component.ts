c34649f81c38b9f4e0599742802aa5eae725590d
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-12-20 10:34:14 +0100

    update

--- src/app/adminCenter/add-categories/add-categories.component.ts
+++ src/app/adminCenter/add-categories/add-categories.component.ts
@@ -19,11 +19,12 @@ export class AddCategoriesComponent implements OnInit{
   ngOnInit() {
   }
 
-  addCategory(category: { category: { name: string; description: string } }){
+  addCategory(category: { name: string, description: string }){
     console.log("category is " + category)
     this.service.addCategory(category)
       .subscribe({
-        next: () => {
+        next: data => {
+          console.log(data);
         },
         error: err => {
           console.log(err);
