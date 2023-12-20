e4f8d7add6943886e62a7ac1160996762c711af0
marouane <uanemaro216@gmail.com>
2023-12-20 10:16:24 +0100

    update

--- src/app/adminCenter/add-categories/add-categories.component.ts
+++ src/app/adminCenter/add-categories/add-categories.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PromotionService} from "../../services/promotionService/promotion.service";
+import {CategorieService} from "../../services/CategorieService/category.service";
 
 @Component({
   selector: 'app-add-categories',
   templateUrl: './add-categories.component.html',
   styleUrls: ['./add-categories.component.css']
 })
-export class AddCategoriesComponent {
+export class AddCategoriesComponent implements OnInit{
 
+  category = {
+    name: '',
+    description: ''
+  }
+  constructor(private service:CategorieService) {
+  }
+
+  ngOnInit() {
+  }
+
+  addCategory(category: { category: { name: string; description: string } }){
+    console.log("category is " + category)
+    this.service.addCategory(category)
+      .subscribe({
+        next: () => {
+        },
+        error: err => {
+          console.log(err);
+        }
+      })
+  }
 }
--- src/app/services/CategorieService/category.service.ts
+++ src/app/services/CategorieService/category.service.ts
@@ -11,7 +11,8 @@ export class CategorieService {
   public getAllcategories(page: number = 1, size: number = 10): Observable<Array<any>> {
     return this.http.get<Array<any>>(`http://localhost:8080/api/v1/categories`)
   }
-  public addcategory(category: any):Observable<any> {
-    return this.http.post<any>(`http://localhost:8080/api/v1/promotions/create`, category)
+  public addCategory(category: any):Observable<any> {
+    console.log(category)
+    return this.http.post<any>(`http://localhost:8080/api/v1/categories/create`, category)
   }
 }
